249f971006e0615cbca9bca86585baea695ddc63
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-12-21 21:39:06 +0000

    updating media query + map + contact form2

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -2,7 +2,7 @@ import React from "react";
 import Form from "../Components/Contato/Form";
 import Head from "../Components/Contato/Head";
 import Hours from "../Components/Contato/Hours";
-import Map from "../Components/Contato/Map/Map";
+import Map from "../Components/Contato/Map";
 import Header from "../Components/Head/Header";
 import Localizacao from "../Components/Head/Localizacao";
 
