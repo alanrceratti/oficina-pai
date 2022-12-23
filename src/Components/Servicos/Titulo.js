958e6c021b27afe62520501333cd12df9bbc2ba8
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-12-23 19:43:11 +0000

    services faq without media query

--- src/Components/Servicos/Titulo.js
+++ src/Components/Servicos/Titulo.js
@@ -1,15 +1,14 @@
 import React from "react";
 import styles from "../Servicos/Titulo.module.css";
+import faq from "./faq.json";
 
 const Titulo = () => {
+	const data = { faq };
 	const [isActive, setIsActive] = React.useState(null);
-	const [isShow, setIsShow] = React.useState("");
+	const [isVisible, setIsVisible] = React.useState(null);
 
-	function handleAnswer() {
-		setIsActive(!isActive);
-	}
-	function handleShow(event) {
-		setIsShow(event.CurrentTarget);
+	function handleShow() {
+		setIsVisible(!isVisible);
 	}
 
 	return (
@@ -19,355 +18,39 @@ const Titulo = () => {
 				para o seu veículo:
 			</h1>
 			<ul>
-				<li>
-					<h2 className={styles.nome_servico}>
-						Geometria e Balanceamento
-					</h2>
-					<ul className={styles.perguntas}>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Por que é importante?
-							</h3>
-							<span className={styles.seta}>⭝</span>
+				{data.faq.map((category) => (
+					<li key={category.category}>
+						<h2 className={styles.nome_servico}>
+							{category.category}
+						</h2>
+						<ul className={styles.perguntas}>
+							{category.questions.map((question) => (
+								<li key={question.question}>
+									<h3
+										onClick={() => {
+											handleShow(!isVisible);
+											setIsActive(question.id);
+										}}
+									>
+										{question.question}
+									</h3>
+									<span className={styles.seta}>⭝</span>
 
-							<p
-								className={`${
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								} `}
-							>
-								O balanceamento e a geometria de um veículo são
-								importantes porque afetam a segurança, o
-								desempenho e a durabilidade do veículo.
-								<br /> <br />
-								O balanceamento de um veículo se refere à
-								distribuição uniforme do peso dos pneus e rodas
-								em todas as direções. Quando um veículo está
-								balanceado corretamente, ele se desloca de forma
-								suave e estável, o que pode ajudar a reduzir o
-								desgaste excessivo dos pneus e a prolongar a
-								vida útil do conjunto de rodas e pneus.
-								<br /> <br />
-								Além disso, um veículo bem balanceado é mais
-								fácil de manobrar e pode oferecer uma
-								experiência de condução mais confortável. A
-								geometria de um veículo se refere à configuração
-								das rodas, suspensão e direção em relação ao
-								veículo. <br /> <br />
-								A geometria correta é essencial para garantir
-								que o veículo se comporte de forma previsível e
-								segura quando estiver em movimento. Por exemplo,
-								se a geometria do veículo estiver desalinhada,
-								isso pode afetar a direção e a estabilidade do
-								veículo, o que pode aumentar o risco de
-								acidentes. <br /> <br />
-								Adicionalmente, a geometria incorreta pode levar
-								ao desgaste anormal dos pneus e a problemas com
-								a suspensão. Em resumo, o balanceamento e a
-								geometria são importantes para garantir que um
-								veículo se desloque de forma segura e eficiente,
-								e para prolongar a vida útil do conjunto de
-								rodas e pneus.
-							</p>
-						</li>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Com que frequência devo verificar o
-								Balanceamento e Geometria do meu veículo?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								A frequência com que o balanceamento e a
-								geometria de um veículo devem ser verificados e
-								corrigidos depende de vários fatores, como o
-								tipo de veículo, o tipo de pneus, o estilo de
-								condução e as condições de estrada. Alguns
-								fabricantes de veículos recomendam verificar o
-								balanceamento e a geometria a cada 10.000 a
-								20.000 km, enquanto outros podem recomendar
-								verificações mais frequentes ou menos
-								frequentes. Alguns sinais comuns de que o
-								balanceamento e a geometria de um veículo podem
-								precisar de atenção incluem: Pneus desgastados
-								de forma desigual ou irregular Vibrações ou
-								trepidação durante a condução Estabilidade
-								reduzida ou direção difícil Pneus que perdem
-								pressão com frequência É importante seguir as
-								recomendações do fabricante do veículo quanto à
-								frequência de verificação do balanceamento e da
-								geometria. Além disso, é uma boa ideia verificar
-								o balanceamento e a geometria sempre que os
-								pneus forem substituídos ou quando o veículo
-								tiver sido submetido a condições de estrada
-								extremas, como buracos ou obstáculos.
-							</p>
-						</li>
-						<li>
-							<h3 onClick={handleAnswer}> Qual o custo médio?</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								O custo médio para o balanceamento e a geometria
-								de um veículo pode variar bastante, dependendo
-								de vários fatores, como o tipo de veículo, o
-								tipo de pneus e a complexidade da tarefa. Aqui
-								na nossa oficina os preço inicial é de R$ 69,00
-							</p>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<h2 className={styles.nome_servico}>
-						Alinhamento de eixo traseiro
-					</h2>
-					<ul className={styles.perguntas}>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Por que é importante?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								O alinhamento de eixo traseiro de um veículo é
-								importante porque afeta a direção, a
-								estabilidade e o desempenho do veículo. Quando
-								um veículo está alinhado corretamente, as rodas
-								traseiras estão alinhadas de forma precisa em
-								relação à direção do veículo e às rodas da
-								frente. Isso pode ajudar a garantir que o
-								veículo se comporte de forma previsível e segura
-								quando estiver em movimento. Um alinhamento
-								incorreto do eixo traseiro pode levar a
-								problemas como: Direção difícil ou instável
-								Desgaste anormal dos pneus Problemas de
-								estabilidade, especialmente em altas velocidades
-								ou em curvas Redução do desempenho e do consumo
-								de combustível Por essas razões, é importante
-								verificar o alinhamento do eixo traseiro de
-								forma regular e corrigir qualquer problema de
-								alinhamento o mais rápido possível. A frequência
-								com que o alinhamento do eixo traseiro deve ser
-								verificado depende do tipo de veículo, do estilo
-								de condução e de outros fatores.
-							</p>
-						</li>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Com que frequência devo verificar o Alinhamento
-								de Eixo traseiro do meu veículo?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-						</li>
-						<li>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								Alguns fabricantes de veículos recomendam
-								verificações a cada 10.000 a 20.000 km, enquanto
-								outros podem recomendar verificações mais
-								frequentes ou menos frequentes. É importante
-								seguir as recomendações do fabricante do veículo
-								quanto à frequência de verificação do
-								alinhamento do eixo traseiro ou se você notar
-								algum dos sinais descritos na pergunta anterior
-								que podem indicar algum problema no eixo
-								traseiro.
-							</p>
-							<h3 onClick={handleAnswer}> Qual o custo médio?</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								O custo médio para o alinhamento de eixo
-								traseiro de um veículo pode variar bastante,
-								dependendo de vários fatores, como o tipo de
-								veículo, o tipo de pneus e a complexidade da
-								tarefa. Aqui na nossa oficina os preço inicial é
-								de R$ 159,00.
-							</p>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<h2 className={styles.nome_servico}>
-						Troca de óleo e filtro
-					</h2>
-					<ul className={styles.perguntas}>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Por que é importante?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-						</li>
-						<li>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								A troca de óleo e filtro é uma manutenção
-								importante para garantir o bom funcionamento do
-								motor do seu veículo. O óleo lubrifica e protege
-								as peças móveis do motor, enquanto o filtro de
-								óleo remove impurezas e sujeira do óleo para
-								garantir que ele continue a funcionar
-								corretamente. Ao trocar o óleo e o filtro
-								regularmente, você pode ajudar a garantir que o
-								motor do seu veículo receba o lubrificação
-								adequada e está protegido contra danos.
-							</p>
-
-							<h3 onClick={handleAnswer}>
-								Com que frequência realizar a troca de óleo e
-								filtro do meu veículo?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-						</li>
-						<li>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								A frequência com que a troca de óleo e filtro
-								deve ser realizada depende do tipo de veículo,
-								do tipo de óleo e de outros fatores. Alguns
-								fabricantes de veículos recomendam trocas a cada
-								5.000 a 10.000 km, enquanto outros podem
-								recomendar intervalos mais longos ou mais
-								curtos. É importante seguir as recomendações do
-								fabricante do veículo quanto à frequência de
-								troca de óleo e filtro. Além disso, é uma boa
-								ideia verificar o nível de óleo e o estado do
-								filtro regularmente e trocar o óleo e o filtro
-								sempre que forem necessários.
-							</p>
-							<h3 onClick={handleAnswer}> Qual o custo médio?</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								O custo da troca de óleo e filtro pode variar
-								bastante, dependendo do tipo de veículo, do tipo
-								de óleo e de outros fatores. Aqui na nossa
-								oficina os preço inicial é de R$ 199,00.
-							</p>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<h2 className={styles.nome_servico}>
-						Manutenção preventiva
-					</h2>
-					<ul className={styles.perguntas}>
-						<li>
-							<h3 onClick={handleAnswer}>
-								Por que é importante?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-						</li>
-						<li>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								A manutenção preventiva é um conjunto de tarefas
-								de manutenção realizadas regularmente para
-								garantir o bom funcionamento e a longevidade de
-								um veículo. O objetivo da manutenção preventiva
-								é identificar e corrigir problemas potenciais
-								antes que eles causem danos ou falhas graves. A
-								manutenção preventiva pode incluir tarefas como:
-								Troca de óleo e filtro de óleo Verificação e
-								ajuste do nível de fluidos do veículo (como
-								freio, refrigerante e transmissão) Troca de
-								componentes desgastados ou danificados (como
-								correias, filtros de ar e velas de ignição)
-								Verificação e ajuste da pressão dos pneus
-								Verificação e ajuste da geometria e do
-								balanceamento das rodas
-							</p>
-
-							<h3 onClick={handleAnswer}>
-								Com que frequência realizar a manutenção
-								preventiva do meu veículo?
-							</h3>
-							<span className={styles.seta}>⭝</span>
-						</li>
-						<li>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								A frequência com que a manutenção preventiva
-								deve ser realizada depende do tipo de veículo,
-								do estilo de condução e de outros fatores.
-								Alguns fabricantes de veículos recomendam
-								verificações e tarefas de manutenção a cada
-								5.000 a 10.000 km, enquanto outros podem
-								recomendar intervalos mais longos ou mais
-								curtos. É importante seguir as recomendações do
-								fabricante do veículo quanto à frequência de
-								manutenção preventiva.
-							</p>
-							<h3 onClick={handleAnswer}> Qual o custo médio?</h3>
-							<span className={styles.seta}>⭝</span>
-							<p
-								className={
-									isActive
-										? styles.respostaActive
-										: styles.resposta
-								}
-							>
-								O custo da manutenção preventiva pode variar
-								bastante, dependendo do tipo de veículo, da
-								complexidade da tarefa, quantidade de
-								componentes a serem substituidos e de outros
-								fatores. Aqui na nossa oficina os preço inicial
-								é de R$ 99,00.
-							</p>
-						</li>
-					</ul>
-				</li>
+									<p
+										className={`${
+											isActive === question.id &&
+											isVisible
+												? styles.respostaActive
+												: styles.resposta
+										} `}
+									>
+										{question.answer}
+									</p>
+								</li>
+							))}
+						</ul>
+					</li>
+				))}
 			</ul>
 		</section>
 	);
