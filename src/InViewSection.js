105a3d4473866aef0861a0a368f80d98b611131c
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-02-12 20:34:45 +0000

    inView Frame motion

---
+++ src/InViewSection.js
@@ -0,0 +1,27 @@
+import { useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+
+function InViewSection({ children }) {
+	const ref = useRef(null);
+	const isInView = useInView(ref);
+
+	useEffect(() => {
+		console.log("Element is in view: ", isInView);
+	}, [isInView]);
+
+	return (
+		<section ref={ref} className="mainContainer">
+			<span
+				style={{
+					transform: isInView ? "none" : "translateX(-200px)",
+					opacity: isInView ? 1 : 0,
+					transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+				}}
+			>
+				{children}
+			</span>
+		</section>
+	);
+}
+
+export default InViewSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,48 +5,25 @@ import Footer from "../Components/Footer";
 import Titulo from "../Components/Header/Titulo";
 import Titulo2 from "../Components/Header/Titulo2";
 import ServicosList from "../Components/Header/ServicosList";
-import { useInView } from "framer-motion";
-
-function Section({ children }) {
-	const ref = useRef(null);
-	const isInView = useInView(ref);
-
-	useEffect(() => {
-		console.log("Element is in view: ", isInView);
-	}, [isInView]);
-
-	return (
-		<section ref={ref} className="mainContainer">
-			<span
-				style={{
-					transform: isInView ? "none" : "translateX(-200px)",
-					opacity: isInView ? 1 : 0,
-					transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-				}}
-			>
-				{children}
-			</span>
-		</section>
-	);
-}
+import InViewSection from "../InViewSection";
 
 export default function Home() {
 	return (
 		<>
-			<Section>
+			<section className="mainContainer">
 				<Header />
-				<Section>
+				<InViewSection>
 					<Titulo />
-				</Section>
-				<Section>
+				</InViewSection>
+				<InViewSection>
 					<Titulo2 />
-				</Section>
-				<Section>
+				</InViewSection>
+				<InViewSection>
 					<ServicosList />
-				</Section>
+				</InViewSection>
 				<Footer />
 				<Localizacao />
-			</Section>
+			</section>
 		</>
 	);
 }
