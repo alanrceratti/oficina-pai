8d9489131563e991b875915775c9c19752f1b2cd
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2023-02-13 20:07:27 +0000

    frame motion fix

--- src/InViewSection.js
+++ src/InViewSection.js
@@ -3,7 +3,10 @@ import React, { useEffect, useRef } from "react";
 
 function InViewSection({ children }) {
 	const ref = useRef(null);
-	const isInView = useInView(ref);
+	const isInView = useInView(ref, {
+		margin: "0px 100px -150px 0px",
+		once: true,
+	});
 
 	useEffect(() => {
 		// console.log("Element is in view: ", isInView);
@@ -13,9 +16,16 @@ function InViewSection({ children }) {
 		<section ref={ref} className="mainContainer">
 			<span
 				style={{
+					display: "flex",
+					width: "100%",
 					transform: isInView ? "none" : "translateX(-200px)",
+					WebkitTransform: isInView ? "none" : "translateX(-200px)",
+					MozTransform: isInView ? "none" : "translateX(-200px)",
+					msTransform: isInView ? "none" : "translateX(-200px)",
+					OTransform: isInView ? "none" : "translateX(-200px)",
 					opacity: isInView ? 1 : 0,
-					transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+
+					transition: "all 1s ",
 				}}
 			>
 				{children}
